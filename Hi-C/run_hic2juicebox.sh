be95afc67b97edbc278ad8bb75416619e22a94ee
JoannaAch <JoannaAch@users.noreply.github.com>
2021-03-23 15:02:15 +1100

    Create run_hic2juicebox.sh

---
+++ Hi-C/run_hic2juicebox.sh
@@ -0,0 +1,16 @@
+#Create .hic files for Juicebox
+
+
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/hicpro_gar15-13d_Decitabine/hic_results/data/Hi-C_PDX_GAR15-13D_Dec4/Hi-C_PDX_GAR15-13D_Dec4_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg38.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/Arima_resfrag_N_hg38.bed -o /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/juicebox
+
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/hicpro_gar15-13d_Decitabine/hic_results/data/Hi-C_PDX_GAR15-13D_Dec5/Hi-C_PDX_GAR15-13D_Dec5_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg38.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/Arima_resfrag_N_hg38.bed -o /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/juicebox
+
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/hicpro_gar15-13d_Decitabine/hic_results/data/Hi-C_PDX_GAR15-13D_Dec9/Hi-C_PDX_GAR15-13D_Dec9_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg38.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/Arima_resfrag_N_hg38.bed -o /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/juicebox
+
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/hicpro_gar15-13d_Vehicle/hic_results/data/Hi-C_PDX_GAR15-13D_Veh1/Hi-C_PDX_GAR15-13D_Veh1_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg38.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/Arima_resfrag_N_hg38.bed -o /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/juicebox
+
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/hicpro_gar15-13d_Vehicle/hic_results/data/Hi-C_PDX_GAR15-13D_Veh2/Hi-C_PDX_GAR15-13D_Veh2_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg38.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/Arima_resfrag_N_hg38.bed -o /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/juicebox
+
+time /home/joaach/HiC-Pro_2/bin/utils/hicpro2juicebox.sh -i /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/hicpro_gar15-13d_Vehicle/hic_results/data/Hi-C_PDX_GAR15-13D_Veh4/Hi-C_PDX_GAR15-13D_Veh4_allValidPairs -g /home/joaach/HiC-Pro_2/annotation/chrom_hg38.sizes -j /home/joaach/juicebox/juicebox_tools.7.5.jar -r /home/joaach/HiC-Pro_2/annotation/Arima_resfrag_N_hg38.bed -o /share/ScratchGeneral/joaach/Hi-C_Level2_Arima_PDX_GAR15-13D/juicebox
+
+echo "done"
