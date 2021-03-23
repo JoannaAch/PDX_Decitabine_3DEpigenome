75fab3bb697266a91e7082ddc56efb880b410dee
JoannaAch <JoannaAch@users.noreply.github.com>
2021-03-23 15:14:24 +1100

    Create digest_arima.sh

---
+++ PCHi-C/digest_arima.sh
@@ -0,0 +1,3 @@
+##in silico digestion of hg38 for hicup wihth Arima design
+
+./hicup_digester --genome genome --arima /share/ClusterShare/biodata/contrib/genomeIndices_garvan/iGenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa
